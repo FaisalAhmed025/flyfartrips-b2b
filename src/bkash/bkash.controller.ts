f87e7a3be32699d05eeebaccf3c3814ab11166c6
mdfaisalahmed025 <md.faisal3@northsouth.edu>
2023-07-24 17:06:53 +0600

    nagad encrypty dycrypt data

--- src/bkash/bkash.controller.ts
+++ src/bkash/bkash.controller.ts
@@ -11,19 +11,21 @@ import { createHash } from 'crypto';
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 import { diffSeconds } from './diffSeconds';
 import { HttpService } from '@nestjs/axios';
-import { retry } from 'rxjs';
+
 
 @Controller('bkash')
 export class BkashController {
   private readonly baseURL = 'https://tokenized.pay.bka.sh/v1.2.0-beta/tokenized';
-  private readonly key = 'gswy5A2C7tHMZCOdoJWEIA5Ntc';
-  private readonly secret = 'WSRHWksUVGTiZHDefwmznOWIr9TNGu7VfkkHWV7NCMfvbRClWO5g';
-  private token: string | null = null;
-  private refreshToken: string | null = null;
+  private readonly app_key = 'gswy5A2C7tHMZCOdoJWEIA5Ntc';
+  private readonly app_secret = 'WSRHWksUVGTiZHDefwmznOWIr9TNGu7VfkkHWV7NCMfvbRClWO5g';
+  private token: string;
+  private refreshToken: string;
   private tokenIssueTime: number = 0;
   private readonly headers = {
     username: '01325087966',
-    password: '+RMPR@ug#4['
+    password: '+RMPR@ug#4[',
+    'Content-Type':"application/json",
+    accept: 'application/json'
   };
   constructor(
     private readonly bkashService: BkashService,
@@ -53,8 +55,8 @@ export class BkashController {
         method: 'POST',
         url: `${this.baseURL}/checkout/token/grant`,
         data: {
-          app_key: this.key,
-          app_secret: this.secret,
+          app_key: this.app_key,
+          app_secret: this.app_secret,
         },
         headers: this.headers,
       };
@@ -67,7 +69,6 @@ export class BkashController {
         // Throw a custom exception indicating that invalid API credentials were provided
         throw new BkashException('Invalid API Credentials Provided');
       }
-  
       // If everything is successful, return the response containing the token
       return response.data.id_token
     } catch (error) {
@@ -79,90 +80,95 @@ export class BkashController {
   }
 
 
-  async newToken(refresh: string): Promise<any> {
-    const requestconfig: AxiosRequestConfig ={
-      method: 'POST',
-      url:`${this.baseURL}/checkout/token/refresh`,
-      data:{
-        app_key: this.key,
-        app_secret: this.secret,
-        refresh_token: refresh,
-      },
-      headers:this.headers
-    };
-    try {
-      const response: AxiosResponse<IBkashTokenResponse> = await axios(requestconfig)
-      return response.data.id_token
-    } catch (error) {
+  // async newToken(refresh: string): Promise<any> {
+  //   const requestconfig: AxiosRequestConfig ={
+  //     method: 'POST',
+  //     url:`${this.baseURL}/checkout/token/refresh`,
+  //     data:{
+  //       app_key: this.key,
+  //       app_secret: this.secret,
+  //       refresh_token: refresh,
+  //     },
+  //     headers:this.headers
+  //   };
+  //   try {
+  //     const response: AxiosResponse<IBkashTokenResponse> = await axios(requestconfig)
+  //     return response.data.id_token
+  //   } catch (error) {
       
-    }
-  }
+  //   }
+  // }
 
-  async getToken(): Promise<string> {
-    if (!this.token) {
-      const { id_token, refresh_token, msg, status } = await this.getInitialToken();
+  // async getToken(): Promise<string> {
+  //   if (!this.token) {
+  //     const { id_token, refresh_token, msg, status } = await this.getInitialToken();
 
-      // Throw an error if bkash sends status [only happens when the request fails]
-      if (status && msg) throw new BkashException(msg);
-      this.token = id_token;
-      this.refreshToken = refresh_token;
-      this.tokenIssueTime = Date.now();
-      return this.token;
-    }
-    const diff = diffSeconds(this.tokenIssueTime);
+  //     // Throw an error if bkash sends status [only happens when the request fails]
+  //     if (status && msg) throw new BkashException(msg);
+  //     this.token = id_token;
+  //     this.refreshToken = refresh_token;
+  //     this.tokenIssueTime = Date.now();
+  //     return this.token;
+  //   }
+  //   const diff = diffSeconds(this.tokenIssueTime);
 
-    if (diff < 3500) {
-      return this.token;
-    }
+  //   if (diff < 3500) {
+  //     return this.token;
+  //   }
 
-    // The token is expired, refresh it
-    const { id_token, refresh_token, msg, status } = await this.newToken(this.refreshToken);
+  //   // The token is expired, refresh it
+  //   const { id_token, refresh_token, msg, status } = await this.newToken(this.refreshToken);
 
-    // Throw an error if bkash sends status [only happens when the request fails]
-    if (status && msg) throw new BkashException(msg);
-    this.token = id_token;
-    this.refreshToken = refresh_token;
-    this.tokenIssueTime = Date.now();
-    return this.token;
-  }
+  //   // Throw an error if bkash sends status [only happens when the request fails]
+  //   if (status && msg) throw new BkashException(msg);
+  //   this.token = id_token;
+  //   this.refreshToken = refresh_token;
+  //   this.tokenIssueTime = Date.now();
+  //   return this.token;
+  // }
 
   
-  async createTokenHeader(){
-    const token = await this.getToken();
-    const header = {
-      authorization: token,
-      accept:'*/*',
-      'x-app-key': this.key,
-    };
-    return header;
-  }
+  // async createTokenHeader(){
+  //   const token = await this.getToken();
+  //   const header = {
+  //     authorization: token,
+  //     accept:'*/*',
+  //     'x-app-key': this.key,
+  //   };
+  //   return header;
+  // }
 
 
   @Post('createPayment')
   async createPayment( @Req() req: Request, @Res() res: Response) {
     const { amount, merchantAssociationInfo, mode,tran_id, payerReference} = req.body;
-    const token = req.headers['authorization']
+    const token = await this.getInitialToken()
     const headers = {
-      Authorization: token,
-      accept:'*/*',
-      'x-app-key': this.key,
-    };
+      'Content-Type':'application/json',
+      'Authorization': token,
+      'x-app-key': this.app_key
+    }
     const paymentID = tran_id || this.generateCustomTransactionId();
     const orderID =  this.generateCustomorderId();
+
     const payload = {
       mode:mode || '',
       payerReference: payerReference || '' ,
       paymentID:paymentID || '',
       callbackURL:"https://flyfarladies.com",
       amount: amount || 0,
-      intent:'Sale',
+      intent:'sale',
       currency: 'BDT',
       merchantInvoiceNumber: orderID, 
-      merchantAssociationInfo: merchantAssociationInfo ||'',
+      merchantAssociationInfo: merchantAssociationInfo || '',
     }
-    const url =`https://tokenized.pay.bka.sh/v1.2.0-beta/tokenized/checkout/payment/create`
-      const Response= await this.httpService.post(url, payload, {headers}).toPromise();
-      return Response.data
+      const url = `https://tokenized.pay.bka.sh/tokenized/checkout/create`
+      console.log(url);
+      console.log(payload);
+      const x= await this.httpService.post(url, payload, {headers})
+      console.log(x);
+      return res.json(x);
+     
   }
 
 @Post('execute/:paymentID')
@@ -170,24 +176,17 @@ async executePayemnt( @Req() req: Request, @Param('paymentID') paymentID :string
   const {customerMsisdn, payerReference, paymentExecuteTime,
      trxID,transactionStatus,amount,currency, intent,merchantInvoiceNumber,
      statusCode,
-     statusMessage
-
+     statusMessage,
     } = req.body
 
     const requestdata ={customerMsisdn, payerReference, paymentExecuteTime,
       trxID,transactionStatus,amount,currency, intent,merchantInvoiceNumber,
       statusCode,
-      statusMessage
- 
+      statusMessage,
+      paymentID
      }
      return requestdata;
 }
-
-
-
-
-
-
 
 }
 
--- src/nagad/nagad.controller.ts
+++ src/nagad/nagad.controller.ts
@@ -1,41 +1,111 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, Req } from '@nestjs/common';
 import { NagadService } from './nagad.service';
 import { CreateNagadDto } from './dto/create-nagad.dto';
 import { UpdateNagadDto } from './dto/update-nagad.dto';
+import * as crypto from 'crypto';
+import { buffer } from 'stream/consumers';
+import { Request, Response } from 'express';
+
 
 @Controller('nagad')
 export class NagadController {
   private readonly baseURL: 'http://sandbox.mynagad.com:10080';
 	private readonly merchantID: 'MC084704';
 	private readonly merchantNumber: ' 01400001104';
-	private readonly pubKey: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAjBH1pFNSSRKPuMcNxmU5jZ1x8K9LPFM4XSu11m7uCfLUSE4SEjL30w3ockFvwAcuJffCUwtSpbjr34cSTD7EFG1Jqk9Gg0fQCKvPaU54jjMJoP2toR9fGmQV7y9fz31UVxSk97AqWZZLJBT2lmv76AgpVV0k0xtb/0VIv8pd/j6TIz9SFfsTQOugHkhyRzzhvZisiKzOAAWNX8RMpG+iqQi4p9W9VrmmiCfFDmLFnMrwhncnMsvlXB8QSJCq2irrx3HG0SJJCbS5+atz+E1iqO8QaPJ05snxv82Mf4NlZ4gZK0Pq/VvJ20lSkR+0nk+s/v3BgIyle78wjZP1vWLU4wIDAQAB';
+	private readonly pubKey:'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAjBH1pFNSSRKPuMcNxmU5jZ1x8K9LPFM4XSu11m7uCfLUSE4SEjL30w3ockFvwAcuJffCUwtSpbjr34cSTD7EFG1Jqk9Gg0fQCKvPaU54jjMJoP2toR9fGmQV7y9fz31UVxSk97AqWZZLJBT2lmv76AgpVV0k0xtb/0VIv8pd/j6TIz9SFfsTQOugHkhyRzzhvZisiKzOAAWNX8RMpG+iqQi4p9W9VrmmiCfFDmLFnMrwhncnMsvlXB8QSJCq2irrx3HG0SJJCbS5+atz+E1iqO8QaPJ05snxv82Mf4NlZ4gZK0Pq/VvJ20lSkR+0nk+s/v3BgIyle78wjZP1vWLU4wIDAQAB';
 	private readonly privKey: 'MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCJakyLqojWTDAVUdNJLvuXhROV+LXymqnukBrmiWwTYnJYm9r5cKHj1hYQRhU5eiy6NmFVJqJtwpxyyDSCWSoSmIQMoO2KjYyB5cDajRF45v1GmSeyiIn0hl55qM8ohJGjXQVPfXiqEB5c5REJ8Toy83gzGE3ApmLipoegnwMkewsTNDbe5xZdxN1qfKiRiCL720FtQfIwPDp9ZqbG2OQbdyZUB8I08irKJ0x/psM4SjXasglHBK5G1DX7BmwcB/PRbC0cHYy3pXDmLI8pZl1NehLzbav0Y4fP4MdnpQnfzZJdpaGVE0oI15lq+KZ0tbllNcS+/4MSwW+afvOw9bazAgMBAAECggEAIkenUsw3GKam9BqWh9I1p0Xmbeo+kYftznqai1pK4McVWW9//+wOJsU4edTR5KXK1KVOQKzDpnf/CU9SchYGPd9YScI3n/HR1HHZW2wHqM6O7na0hYA0UhDXLqhjDWuM3WEOOxdE67/bozbtujo4V4+PM8fjVaTsVDhQ60vfv9CnJJ7dLnhqcoovidOwZTHwG+pQtAwbX0ICgKSrc0elv8ZtfwlEvgIrtSiLAO1/CAf+uReUXyBCZhS4Xl7LroKZGiZ80/JE5mc67V/yImVKHBe0aZwgDHgtHh63/50/cAyuUfKyreAH0VLEwy54UCGramPQqYlIReMEbi6U4GC5AQKBgQDfDnHCH1rBvBWfkxPivl/yNKmENBkVikGWBwHNA3wVQ+xZ1Oqmjw3zuHY0xOH0GtK8l3Jy5dRL4DYlwB1qgd/Cxh0mmOv7/C3SviRk7W6FKqdpJLyaE/bqI9AmRCZBpX2PMje6Mm8QHp6+1QpPnN/SenOvoQg/WWYM1DNXUJsfMwKBgQCdtddE7A5IBvgZX2o9vTLZY/3KVuHgJm9dQNbfvtXw+IQfwssPqjrvoU6hPBWHbCZl6FCl2tRh/QfYR/N7H2PvRFfbbeWHw9+xwFP1pdgMug4cTAt4rkRJRLjEnZCNvSMVHrri+fAgpv296nOhwmY/qw5Smi9rMkRY6BoNCiEKgQKBgAaRnFQFLF0MNu7OHAXPaW/ukRdtmVeDDM9oQWtSMPNHXsx+crKY/+YvhnujWKwhphcbtqkfj5L0dWPDNpqOXJKV1wHt+vUexhKwus2mGF0flnKIPG2lLN5UU6rs0tuYDgyLhAyds5ub6zzfdUBG9Gh0ZrfDXETRUyoJjcGChC71AoGAfmSciL0SWQFU1qjUcXRvCzCK1h25WrYS7E6pppm/xia1ZOrtaLmKEEBbzvZjXqv7PhLoh3OQYJO0NM69QMCQi9JfAxnZKWx+m2tDHozyUIjQBDehve8UBRBRcCnDDwU015lQN9YNb23Fz+3VDB/LaF1D1kmBlUys3//r2OV0Q4ECgYBnpo6ZFmrHvV9IMIGjP7XIlVa1uiMCt41FVyINB9SJnamGGauW/pyENvEVh+ueuthSg37e/l0Xu0nm/XGqyKCqkAfBbL2Uj/j5FyDFrpF27PkANDo99CdqL5A4NQzZ69QRlCQ4wnNCq6GsYy2WEJyU2D+K8EBSQcwLsrI7QL7fvQ==';
-
 	private readonly callbackURL: string;
   constructor(private readonly nagadService: NagadService) {}
 
-  @Post()
-  create(@Body() createNagadDto: CreateNagadDto) {
-    return this.nagadService.create(createNagadDto);
+  
+
+
+generateRandomString(length = 50) {
+  const characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const charactersLength = characters.length;
+  let randomString = '';
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charactersLength);
+    randomString += characters.charAt(randomIndex);
+  }
+  return randomString;
+}
+
+@Post('encryption')
+  encryptDataWithPublicKey(@Body() data: any): string {
+    const pgPublicKey =
+      'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAiCWvxDZZesS1g1lQfilVt8l3X5aMbXg5WOCYdG7q5C+Qevw0upm3tyYiKIwzXbqexnPNTHwRU7Ul7t8jP6nNVS/jLm35WFy6G9qRyXqMc1dHlwjpYwRNovLc12iTn1C5lCqIfiT+B/O/py1eIwNXgqQf39GDMJ3SesonowWioMJNXm3o80wscLMwjeezYGsyHcrnyYI2LnwfIMTSVN4T92Yy77SmE8xPydcdkgUaFxhK16qCGXMV3mF/VFx67LpZm8Sw3v135hxYX8wG1tCBKlL4psJF4+9vSy4W+8R5ieeqhrvRH+2MKLiKbDnewzKonFLbn2aKNrJefXYY7klaawIDAQAB';
+    const public_key = `-----BEGIN PUBLIC KEY-----\n${pgPublicKey}\n-----END PUBLIC KEY-----`;
+    const bufferData = Buffer.from(JSON.stringify(data), 'utf8');
+    const encryptedData = crypto.publicEncrypt(
+      { key: public_key, padding: crypto.constants.RSA_PKCS1_PADDING },
+      bufferData,
+    );
+    return encryptedData.toString('base64');
   }
 
-  @Get()
-  findAll() {
-    return this.nagadService.findAll();
-  }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.nagadService.findOne(+id);
-  }
+  
+  @Post('generateSignature')
+ async generateSignature(@Req() req: Request) {
+    try {
+     const pgprivateKey =' MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCJakyLqojWTDAVUdNJLvuXhROV+LXymqnukBrmiWwTYnJYm9r5cKHj1hYQRhU5eiy6NmFVJqJtwpxyyDSCWSoSmIQMoO2KjYyB5cDajRF45v1GmSeyiIn0hl55qM8ohJGjXQVPfXiqEB5c5REJ8Toy83gzGE3ApmLipoegnwMkewsTNDbe5xZdxN1qfKiRiCL720FtQfIwPDp9ZqbG2OQbdyZUB8I08irKJ0x/psM4SjXasglHBK5G1DX7BmwcB/PRbC0cHYy3pXDmLI8pZl1NehLzbav0Y4fP4MdnpQnfzZJdpaGVE0oI15lq+KZ0tbllNcS+/4MSwW+afvOw9bazAgMBAAECggEAIkenUsw3GKam9BqWh9I1p0Xmbeo+kYftznqai1pK4McVWW9//+wOJsU4edTR5KXK1KVOQKzDpnf/CU9SchYGPd9YScI3n/HR1HHZW2wHqM6O7na0hYA0UhDXLqhjDWuM3WEOOxdE67/bozbtujo4V4+PM8fjVaTsVDhQ60vfv9CnJJ7dLnhqcoovidOwZTHwG+pQtAwbX0ICgKSrc0elv8ZtfwlEvgIrtSiLAO1/CAf+uReUXyBCZhS4Xl7LroKZGiZ80/JE5mc67V/yImVKHBe0aZwgDHgtHh63/50/cAyuUfKyreAH0VLEwy54UCGramPQqYlIReMEbi6U4GC5AQKBgQDfDnHCH1rBvBWfkxPivl/yNKmENBkVikGWBwHNA3wVQ+xZ1Oqmjw3zuHY0xOH0GtK8l3Jy5dRL4DYlwB1qgd/Cxh0mmOv7/C3SviRk7W6FKqdpJLyaE/bqI9AmRCZBpX2PMje6Mm8QHp6+1QpPnN/SenOvoQg/WWYM1DNXUJsfMwKBgQCdtddE7A5IBvgZX2o9vTLZY/3KVuHgJm9dQNbfvtXw+IQfwssPqjrvoU6hPBWHbCZl6FCl2tRh/QfYR/N7H2PvRFfbbeWHw9+xwFP1pdgMug4cTAt4rkRJRLjEnZCNvSMVHrri+fAgpv296nOhwmY/qw5Smi9rMkRY6BoNCiEKgQKBgAaRnFQFLF0MNu7OHAXPaW/ukRdtmVeDDM9oQWtSMPNHXsx+crKY/+YvhnujWKwhphcbtqkfj5L0dWPDNpqOXJKV1wHt+vUexhKwus2mGF0flnKIPG2lLN5UU6rs0tuYDgyLhAyds5ub6zzfdUBG9Gh0ZrfDXETRUyoJjcGChC71AoGAfmSciL0SWQFU1qjUcXRvCzCK1h25WrYS7E6pppm/xia1ZOrtaLmKEEBbzvZjXqv7PhLoh3OQYJO0NM69QMCQi9JfAxnZKWx+m2tDHozyUIjQBDehve8UBRBRcCnDDwU015lQN9YNb23Fz+3VDB/LaF1D1kmBlUys3//r2OV0Q4ECgYBnpo6ZFmrHvV9IMIGjP7XIlVa1uiMCt41FVyINB9SJnamGGauW/pyENvEVh+ueuthSg37e/l0Xu0nm/XGqyKCqkAfBbL2Uj/j5FyDFrpF27PkANDo99CdqL5A4NQzZ69QRlCQ4wnNCq6GsYy2WEJyU2D+K8EBSQcwLsrI7QL7fvQ=='
+      const mar_pri_key =`-----BEGIN PRIVATE KEY-----\n${pgprivateKey}\n-----END PRIVATE KEY-----`;
+      const privateKey = crypto.createPrivateKey({
+        key: mar_pri_key
+      });
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateNagadDto: UpdateNagadDto) {
-    return this.nagadService.update(+id, updateNagadDto);
-  }
+      // Convert the plain sensitive data to a string
+      const plainSensitiveData = JSON.stringify(req.body);
+      console.log(plainSensitiveData);
+      // Create the signature using SHA1withRSA algorithm
+      const signer = crypto.createSign('RSA-SHA1');
+      signer.update(plainSensitiveData);
+      const signature = signer.sign(privateKey, 'base64');
+      return signature;
+    } catch (error) {
+      console.error('Signature generation error:', error);
+      throw new Error('Failed to generate the signature.');
+    }
+    
+ }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.nagadService.remove(+id);
+
+
+@Post('encrypt')
+async encrypt(@Body() data:any) {
+  const pgPublicKey =
+  'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAiCWvxDZZesS1g1lQfilVt8l3X5aMbXg5WOCYdG7q5C+Qevw0upm3tyYiKIwzXbqexnPNTHwRU7Ul7t8jP6nNVS/jLm35WFy6G9qRyXqMc1dHlwjpYwRNovLc12iTn1C5lCqIfiT+B/O/py1eIwNXgqQf39GDMJ3SesonowWioMJNXm3o80wscLMwjeezYGsyHcrnyYI2LnwfIMTSVN4T92Yy77SmE8xPydcdkgUaFxhK16qCGXMV3mF/VFx67LpZm8Sw3v135hxYX8wG1tCBKlL4psJF4+9vSy4W+8R5ieeqhrvRH+2MKLiKbDnewzKonFLbn2aKNrJefXYY7klaawIDAQAB';
+const public_key = `-----BEGIN PUBLIC KEY-----\n${pgPublicKey}\n-----END PUBLIC KEY-----`;
+  const signerObject = crypto.publicEncrypt(
+    { key:public_key, padding: crypto.constants.RSA_PKCS1_PADDING },
+    Buffer.from(JSON.stringify(data))
+  );
+  return signerObject.toString('base64');
+}
+
+
+@Post('decrypt')
+async decrypt(@Req() req:Request) {
+  try {
+    const pgprivateKey = 'MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCJakyLqojWTDAVUdNJLvuXhROV+LXymqnukBrmiWwTYnJYm9r5cKHj1hYQRhU5eiy6NmFVJqJtwpxyyDSCWSoSmIQMoO2KjYyB5cDajRF45v1GmSeyiIn0hl55qM8ohJGjXQVPfXiqEB5c5REJ8Toy83gzGE3ApmLipoegnwMkewsTNDbe5xZdxN1qfKiRiCL720FtQfIwPDp9ZqbG2OQbdyZUB8I08irKJ0x/psM4SjXasglHBK5G1DX7BmwcB/PRbC0cHYy3pXDmLI8pZl1NehLzbav0Y4fP4MdnpQnfzZJdpaGVE0oI15lq+KZ0tbllNcS+/4MSwW+afvOw9bazAgMBAAECggEAIkenUsw3GKam9BqWh9I1p0Xmbeo+kYftznqai1pK4McVWW9//+wOJsU4edTR5KXK1KVOQKzDpnf/CU9SchYGPd9YScI3n/HR1HHZW2wHqM6O7na0hYA0UhDXLqhjDWuM3WEOOxdE67/bozbtujo4V4+PM8fjVaTsVDhQ60vfv9CnJJ7dLnhqcoovidOwZTHwG+pQtAwbX0ICgKSrc0elv8ZtfwlEvgIrtSiLAO1/CAf+uReUXyBCZhS4Xl7LroKZGiZ80/JE5mc67V/yImVKHBe0aZwgDHgtHh63/50/cAyuUfKyreAH0VLEwy54UCGramPQqYlIReMEbi6U4GC5AQKBgQDfDnHCH1rBvBWfkxPivl/yNKmENBkVikGWBwHNA3wVQ+xZ1Oqmjw3zuHY0xOH0GtK8l3Jy5dRL4DYlwB1qgd/Cxh0mmOv7/C3SviRk7W6FKqdpJLyaE/bqI9AmRCZBpX2PMje6Mm8QHp6+1QpPnN/SenOvoQg/WWYM1DNXUJsfMwKBgQCdtddE7A5IBvgZX2o9vTLZY/3KVuHgJm9dQNbfvtXw+IQfwssPqjrvoU6hPBWHbCZl6FCl2tRh/QfYR/N7H2PvRFfbbeWHw9+xwFP1pdgMug4cTAt4rkRJRLjEnZCNvSMVHrri+fAgpv296nOhwmY/qw5Smi9rMkRY6BoNCiEKgQKBgAaRnFQFLF0MNu7OHAXPaW/ukRdtmVeDDM9oQWtSMPNHXsx+crKY/+YvhnujWKwhphcbtqkfj5L0dWPDNpqOXJKV1wHt+vUexhKwus2mGF0flnKIPG2lLN5UU6rs0tuYDgyLhAyds5ub6zzfdUBG9Gh0ZrfDXETRUyoJjcGChC71AoGAfmSciL0SWQFU1qjUcXRvCzCK1h25WrYS7E6pppm/xia1ZOrtaLmKEEBbzvZjXqv7PhLoh3OQYJO0NM69QMCQi9JfAxnZKWx+m2tDHozyUIjQBDehve8UBRBRcCnDDwU015lQN9YNb23Fz+3VDB/LaF1D1kmBlUys3//r2OV0Q4ECgYBnpo6ZFmrHvV9IMIGjP7XIlVa1uiMCt41FVyINB9SJnamGGauW/pyENvEVh+ueuthSg37e/l0Xu0nm/XGqyKCqkAfBbL2Uj/j5FyDFrpF27PkANDo99CdqL5A4NQzZ69QRlCQ4wnNCq6GsYy2WEJyU2D+K8EBSQcwLsrI7QL7fvQ=='; // Replace with your actual private key
+    const mar_pri_key = `-----BEGIN PRIVATE KEY-----\n${pgprivateKey}\n-----END PRIVATE KEY-----`;
+   const x  = req.headers['authorization']
+    const decryptedData = crypto.privateDecrypt(
+      {
+        key: mar_pri_key,
+        padding: crypto.constants.RSA_PKCS1_PADDING,
+      },
+      Buffer.from(x, 'base64')
+    );
+    return JSON.parse(decryptedData.toString());
+  } catch (error) {
+    console.error('Decryption error:', error);
+    throw new Error('Failed to decrypt the data.');
   }
 }
+}
+
+
+
+
