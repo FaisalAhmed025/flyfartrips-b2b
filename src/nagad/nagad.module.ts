dce4c452b072627b9eb67fd7c5de793322df1af8
mdfaisalahmed025 <md.faisal3@northsouth.edu>
2023-07-26 17:12:09 +0600

    partial nagad done

--- src/nagad/nagad.module.ts
+++ src/nagad/nagad.module.ts
@@ -1,9 +1,18 @@
-import { Module } from '@nestjs/common';
+
+
 import { NagadService } from './nagad.service';
 import { NagadController } from './nagad.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [
+    HttpModule.register({
+    }),
+  ],
   controllers: [NagadController],
-  providers: [NagadService]
+  providers: [NagadService],
+ 
 })
 export class NagadModule {}
